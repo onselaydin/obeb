bac87e628b120cded5680939211128ce32447286
Onsel AYDIN <onselaydin@onsels-mac.local>
2016-01-14 02:11:13 -0800

    Initial Commit

---
+++ obeb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  obeb
+//
+//  Created by Onsel AYDIN on 14/01/16.
+//  Copyright © 2016 Onsel AYDIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
